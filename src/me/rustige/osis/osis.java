8be5b12d772a8da2122ab84dd341085c0b10251f
Christoph <christoph@rustige.me>
2023-08-15 01:47:00 +0200

    implemented help command
    - implemented help command with short tutorial
    - major restructuring of main method

--- src/me/rustige/osis/osis.java
+++ src/me/rustige/osis/osis.java
@@ -10,15 +10,24 @@ public class osis {
     public static int urcount;
 
     public static void main (String[] args) {
+        launchScanner();
+    }
+
+    public static void launchScanner(){
         System.out.println("Please enter subnet to be scanned:");
+        System.out.println("Or enter 'help' to see how to use OSIS.");
         subnet = getInput.nextLine();
-        System.out.println("I'm going to start scanning. If nothing happens, that probably means I'm working hard, so please be patient.");
-        System.out.println("Detected IPs:");
-        try{
-            netScanner.checkHosts(subnet);
-        }catch(Exception e){
+        if (subnet.equals("help"))
+        {
+            helpCommand.showHelp();
+        }else{
+            System.out.println("I'm going to start scanning. If nothing happens, that probably means I'm working hard, so please be patient.");
+            System.out.println("Detected IPs:");
+            try{
+                netScanner.checkHosts(subnet);
+            }catch(Exception e){
 
+            }
         }
-
     }
 }
